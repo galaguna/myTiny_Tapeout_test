382517c4bda535e48fdd991a7585cea86b06bae5
Gerardo Abel Laguna-Sanchez <65410505+galaguna@users.noreply.github.com>
2025-12-12 10:35:55 -0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -47,8 +47,9 @@ async def test_project(dut):
 
     #SCK period #1:
     signal_a = LogicArray(dut.ui_in.value)
-    signal_b = LogicArray(MSK_SPI_CS_TO_OFF & MSK_SPI_MOSI_TO_OFF) 
-    #word =  signal_a & signal_b 
+    signal_b = LogicArray(MSK_SPI_CS_TO_OFF)  
+    signal_c = LogicArray(MSK_SPI_MOSI_TO_OFF) 
+    word =  signal_a & signal_b & signal_c 
     #dut.ui_in.value = word #
     await ClockCycles(dut.clk, 1)
     ###SCK falling edge:
