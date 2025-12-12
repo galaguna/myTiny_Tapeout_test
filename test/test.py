252c255360432030ccee90649142b7629ee122be
Gerardo Abel Laguna-Sanchez <65410505+galaguna@users.noreply.github.com>
2025-12-12 11:07:26 -0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -49,9 +49,9 @@ async def test_project(dut):
     dut.ui_in.value = dut.ui_in.value.integer &   MSK_SPI_CS_TO_OFF  &   MSK_SPI_MOSI_TO_OFF
     await ClockCycles(dut.clk, 1)
     ###SCK falling edge:
-    #dut.ui_in.value = dut.ui_in.value &  MSK_SPI_SCK_TO_OFF
+    dut.ui_in.value = dut.ui_in.value.integer &  MSK_SPI_SCK_TO_OFF
     await ClockCycles(dut.clk, 4)
     ###SCK rising edge:
-    #dut.ui_in.value = dut.ui_in.value |  MSK_SPI_SCK_TO_ON
+    dut.ui_in.value = dut.ui_in.value.integer |  MSK_SPI_SCK_TO_ON
     await ClockCycles(dut.clk, 4)
     
